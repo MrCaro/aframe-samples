a9145229634b690e721e0faa6b5cfa5f2761cc09
MrCaro <36524539+MrCaro@users.noreply.github.com>
2022-04-22 14:29:23 -0400

    fix: update project git url path

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+# cd public
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:MrCaro/aframe-samples.git master:gh-pages
+cd -
